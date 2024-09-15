18368041f50e099f589e79ae825f88ada64f54d3
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-09-15 19:07:35 -0300

    feat(data): base 64 encoding & decoding. 1.22MB -> 0.78MB 0.

--- data.py
+++ data.py
@@ -172,6 +172,83 @@ result_dict = {
 }
 
 
+charmap = {
+    "000000": "A",
+    "000001": "B",
+    "000010": "C",
+    "000011": "D",
+    "000100": "E",
+    "000101": "F",
+    "000110": "G",
+    "000111": "H",
+    "001000": "I",
+    "001001": "J",
+    "001010": "K",
+    "001011": "L",
+    "001100": "M",
+    "001101": "N",
+    "001110": "O",
+    "001111": "P",
+    "010000": "Q",
+    "010001": "R",
+    "010010": "S",
+    "010011": "T",
+    "010100": "U",
+    "010101": "V",
+    "010110": "W",
+    "010111": "X",
+    "011000": "Y",
+    "011001": "Z",
+    "011010": "a",
+    "011011": "b",
+    "011100": "c",
+    "011101": "d",
+    "011110": "e",
+    "011111": "f",
+    "100000": "g",
+    "100001": "h",
+    "100010": "i",
+    "100011": "j",
+    "100100": "k",
+    "100101": "l",
+    "100110": "m",
+    "100111": "n",
+    "101000": "o",
+    "101001": "p",
+    "101010": "q",
+    "101011": "r",
+    "101100": "s",
+    "101101": "t",
+    "101110": "u",
+    "101111": "v",
+    "110000": "w",
+    "110001": "x",
+    "110010": "y",
+    "110011": "z",
+    "110100": "0",
+    "110101": "1",
+    "110110": "2",
+    "110111": "3",
+    "111000": "4",
+    "111001": "5",
+    "111010": "6",
+    "111011": "7",
+    "111100": "8",
+    "111101": "9",
+    "111110": "+",
+    "111111": "/",
+}
+
+semicharmap = {
+    0: "000",
+    1: "001",
+    2: "010",
+    3: "011",
+    4: "100",
+    5: "101",
+    6: "110",
+}
+
 def output_json():
     running = False
     run_flag = False
@@ -196,7 +273,34 @@ def output_json():
                 result_dict["paths"][-1] += 1
 
     # Compression run
-    result_dict["paths"] = ",".join([str(x) for x in result_dict["paths"]])
+    paths = ""
+
+    for path in result_dict["paths"]:
+        if type(path) is int:
+            paths += f",{path}"
+        else:
+            binarypath = path[0]
+
+            if len(path) <= 1:
+                paths += f",{binarypath}"
+                continue
+
+            # for each pair of characters
+            for i in range(1, len(path) - 1, 2):
+                left = semicharmap[int(path[i])]
+                right = semicharmap[int(path[i + 1])]
+                bc = left + right
+                binarypath += charmap[bc]
+            
+            if len(binarypath) % 2 != 0:
+                left = semicharmap[int(path[-1])]
+                bc = left + "111"
+                binarypath += charmap[bc]
+
+            paths += f",{binarypath}"
+
+
+    result_dict["paths"] = paths[1:]
 
     with open("output.json", mode="w", encoding="utf-8") as f:
         json.dump(result_dict, f, separators=(",", ":"))
--- script.js
+++ script.js
@@ -1,5 +1,83 @@
 let unicodeData = null;
 
+const inverseCharmap = {
+	A: "000000",
+	B: "000001",
+	C: "000010",
+	D: "000011",
+	E: "000100",
+	F: "000101",
+	G: "000110",
+	H: "000111",
+	I: "001000",
+	J: "001001",
+	K: "001010",
+	L: "001011",
+	M: "001100",
+	N: "001101",
+	O: "001110",
+	P: "001111",
+	Q: "010000",
+	R: "010001",
+	S: "010010",
+	T: "010011",
+	U: "010100",
+	V: "010101",
+	W: "010110",
+	X: "010111",
+	Y: "011000",
+	Z: "011001",
+	a: "011010",
+	b: "011011",
+	c: "011100",
+	d: "011101",
+	e: "011110",
+	f: "011111",
+	g: "100000",
+	h: "100001",
+	i: "100010",
+	j: "100011",
+	k: "100100",
+	l: "100101",
+	m: "100110",
+	n: "100111",
+	o: "101000",
+	p: "101001",
+	q: "101010",
+	r: "101011",
+	s: "101100",
+	t: "101101",
+	u: "101110",
+	v: "101111",
+	w: "110000",
+	x: "110001",
+	y: "110010",
+	z: "110011",
+	0: "110100",
+	1: "110101",
+	2: "110110",
+	3: "110111",
+	4: "111000",
+	5: "111001",
+	6: "111010",
+	7: "111011",
+	8: "111100",
+	9: "111101",
+	"+": "111110",
+	"/": "111111",
+};
+
+const inverseSemiCharmap = {
+	"000": "0",
+	"001": "1",
+	"010": "2",
+	"011": "3",
+	"100": "4",
+	"101": "5",
+	"110": "6",
+    "111": "",
+};
+
 async function loadJSON() {
 	try {
 		const response = await fetch("output.json", {
@@ -14,25 +92,37 @@ async function loadJSON() {
 
 		const json = await response.json();
 
-        const paths = {};
-        const splitPaths = json["paths"].split(',');
-        let index = 0;
+		const paths = {};
+		const splitPaths = json["paths"].split(",");
+		let index = 0;
 
-        // 
-        for (let i = 0; i < splitPaths.length; i++) {
-            const value = splitPaths[i];
-            // console.log(value, index, parseInt(value));
-            if (!isNaN(parseInt(value))) {
-                index += parseInt(value);
-            } else {
-                paths[index.toString()] = value;
-                index += 1;
-            }
-        }
+		for (let i = 0; i < splitPaths.length; i++) { // splitPaths.length
+			const value = splitPaths[i];
 
-        json["paths"] = paths;
+			if (!isNaN(parseInt(value))) {
+				index += parseInt(value);
+			} else {
+                let newValue = ""
+                newValue += value.charAt(0);
+
+                for (let j = 1; j < value.length; j++) {
+                    const inverse = inverseCharmap[value[j]];
+                    const left = inverse.slice(0, 3);
+                    const right = inverse.slice(3, 6);
+                    newValue += inverseSemiCharmap[left] + inverseSemiCharmap[right];
+                }
+                
+                paths[index.toString()] = newValue;
+
+				index += 1;
+			}
+		}
+
+		json["paths"] = paths;
 
 		unicodeData = json;
+
+        console.log("Data loaded")
 	} catch (error) {
 		document.getElementById("result").textContent =
 			"Error loading JSON: " + error.message;
@@ -43,24 +133,24 @@ loadJSON();
 
 function lookupUnicode(character) {
 	const opcodes = unicodeData["paths"][character.codePointAt(0)];
-    if (!opcodes) {
-        return "Character not found.";
-    }
+	if (!opcodes) {
+		return "Character not found.";
+	}
 
-    const inner = unicodeData["initialKeys"][opcodes.charAt(0)];
-    let result = inner;
+	const inner = unicodeData["initialKeys"][opcodes.charAt(0)];
+	let result = inner;
 
-    const outer = opcodes.split('');
-    outer.shift();
+	const outer = opcodes.split("");
+	outer.shift();
 
-    for (let i = 0; i < outer.length; i++) {
-        const oplist = unicodeData["operations"][outer[i]];
-        for (let j = 0; j < oplist.length; j++) {
-            result = `${oplist[j]}(${result})`
-        }
-    }
+	for (let i = 0; i < outer.length; i++) {
+		const oplist = unicodeData["operations"][outer[i]];
+		for (let j = 0; j < oplist.length; j++) {
+			result = `${oplist[j]}(${result})`;
+		}
+	}
 
-    result = `chr(${result})`
+	result = `chr(${result})`;
 	return result ? result : "Generation script not found.";
 }
 
